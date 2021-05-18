3127584b70abac67ac13e92c0cfc62033456907d
SpideyFanDan <dsmith.teach@gmail.com>
2021-05-18 00:54:57 -0400

    changed colors of letters and added drop shadow

--- src/components/Letters/index.jsx
+++ src/components/Letters/index.jsx
@@ -8,13 +8,22 @@ export const Item = styled.div`
   display: flex;
   justify-content: center;
   padding: .5rem;
-  background-color: #FBBC03;
+  background-color: #C4C4C4;
   color: black;
-  font-size: 24px;
+  font-size: 28px;
   font-weight: bold;
-  border: 5px solid;
-  border-color: black;
+  border: 3px solid;
+  border-color: gray;
   border-radius: 15px;
+   
+  box-shadow:
+  0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+  0 6.7px 5.3px rgba(0, 0, 0, 0.048),
+  0 12.5px 10px rgba(0, 0, 0, 0.06),
+  0 22.3px 17.9px rgba(0, 0, 0, 0.072),
+  0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+  0 100px 80px rgba(0, 0, 0, 0.12)
+;
   `;
 
 export const Grid = styled.div`
