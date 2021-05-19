f2fd5155a7c5a9e30a60532bcd92f71f5ad4c5c9
SpideyFanDan <dsmith.teach@gmail.com>
2021-05-19 12:26:53 -0600

    added Hugo as favicon in public folder

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -15,15 +15,14 @@ export default function NavBar({ user, setUser }) {
   return (
     <>
     <nav className='navbar'>
-      <Link to='/' className='navbar-logo'>Type Tigers</Link>
+      <Link to='/home' className='navbar-logo'>Bearcabulary</Link>
       <div className='menu-icon' onClick={handleClick}>
         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
       </div>
       <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-      <li className='nav-item'><Link to="/about" className='nav-links' onClick={closeMobileMenu}>About</Link></li>
+      <li className='nav-item'><Link to="/home" className='nav-links' onClick={closeMobileMenu}>Home</Link></li>
       <li className='nav-item'><Link to="/dashboard" className='nav-links' onClick={closeMobileMenu}>Dashboard</Link>
       </li>
-      <li className='nav-item'><Link to="/game" className='nav-links' onClick={closeMobileMenu}>Game</Link></li>
       <li className='nav-item'><Link to="" onClick={handleLogOut} className='nav-links-mobile'>Log Out</Link></li></ul>
       
     </nav> </>
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 import AuthPage from "../AuthPage/AuthPage";
 import Game from "../Game";
 import Dashboard from "../Dashboard/Dashboard";
-import Practice from "../Practice/Practice";
+import Home from "../Home/Home";
 import NavBar from "../../components/NavBar/NavBar";
 import styled from "styled-components";
 const Main = styled.div`
@@ -23,6 +23,9 @@ export default function App() {
         <>
           <NavBar user={user} setUser={setUser} />
           <Switch>
+            <Route path="/home">
+              <Home />
+            </Route>
             <Route path="/game">
               <Game />
             </Route>
