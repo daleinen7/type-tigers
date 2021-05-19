36095e72cfe423742ef92a9e4fcdeb3c49b741c6
Kaung Shein <nicholasling1412@gmail.com>
2021-05-18 20:16:22 -0400

    Refactored array to object for feeding data

---
+++ src/components/Image/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Image = ({ image }) => {
+  return (
+    <>
+      <img src={image} alt="test" width="200" />
+    </>
+  );
+};
+
+export default Image;
--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -5,6 +5,7 @@ import update from "immutability-helper";
 import FlashWord from "../../components/FlashWord";
 import UserAnswer from "../../components/UserAnswer";
 import ErrorWords from "../../components/ErrorWords";
+import Image from "../../components/Image";
 
 const StyledDiv = styled.div`
   background: #ffffff;
@@ -13,8 +14,37 @@ const StyledDiv = styled.div`
 
 export default function Game() {
   const [flashWord, setFlashWord] = useState("");
+  const [image, setImage] = useState("");
   const [errorWords, setErrorWords] = useState([]);
-  const testArr = ["five", "two", "three", "four", "five"];
+  const testArr = ["five", "two", "three"];
+  const testLevel = [
+    {
+      word: "Small",
+      sentence: "Brian the Bunny is very small in size and hard to find!",
+      image: "https://nerdist.com/wp-content/uploads/2020/07/maxresdefault.jpg",
+    },
+    {
+      word: "Long",
+      sentence: "Brian the bunny has very long, fluffy ears.",
+      image: "https://nerdist.com/wp-content/uploads/2020/07/maxresdefault.jpg",
+    },
+    {
+      word: "Warm",
+      sentence: "He likes warm places and went to the desert.",
+      image: "https://nerdist.com/wp-content/uploads/2020/07/maxresdefault.jpg",
+    },
+    {
+      word: null,
+      sentence:
+        "After finding the clues, Mr. Bear went to the desert to find Mr. Pickles",
+      image: "https://nerdist.com/wp-content/uploads/2020/07/maxresdefault.jpg",
+    },
+    {
+      word: "Warm",
+      sentence: "He likes warm places and went to the desert.",
+      image: "https://nerdist.com/wp-content/uploads/2020/07/maxresdefault.jpg",
+    },
+  ];
   const [correct, setCorrect] = useState(false);
   const [arrCount, setArrCount] = useState(0);
   const arrCounter = useRef(arrCount);
@@ -47,26 +77,28 @@ export default function Game() {
   };
 
   const handleNextWord = () => {
-    const newCounter = arrCount + 1;
-    setArrCount(newCounter);
-    arrCounter.current = newCounter;
-    setFlashWord(testArr[arrCounter.current]);
-    setCorrect(false);
-    setErrorWords([]);
-    setClicked([]);
-    setLetterCount(0);
-    counter.current = 0;
-    setWordTimer(false);
+    if (arrCounter.current < testLevel.length - 1) {
+      const newCounter = arrCount + 1;
+      setArrCount(newCounter);
+      arrCounter.current = newCounter;
+      setFlashWord(testArr[arrCounter.current]);
+      setCorrect(false);
+      setErrorWords([]);
+      setClicked([]);
+      setLetterCount(0);
+      counter.current = 0;
+      setWordTimer(false);
+    }
   };
 
   useEffect(() => {
-    setFlashWord(testArr[arrCounter.current]);
+    setFlashWord(testLevel[arrCounter.current].word);
+    setImage(testLevel[arrCounter.current].image);
   });
 
   return (
     <StyledDiv>
-      <h1>Spell the word you see below</h1>
-      {/* <Picture/> */}
+      <Image image={image} />
       <FlashWord
         flashWord={flashWord}
         setFlashWord={setFlashWord}
@@ -75,17 +107,19 @@ export default function Game() {
         setWordTimer={setWordTimer}
       />
       <ErrorWords errorWords={errorWords} />
-      <UserAnswer
-        flashWord={flashWord}
-        compareAnswer={compareAnswer}
-        correct={correct}
-        handleNextWord={handleNextWord}
-        counter={counter}
-        clicked={clicked}
-        setClicked={setClicked}
-        letterCount={letterCount}
-        setLetterCount={setLetterCount}
-      />
+      {flashWord && (
+        <UserAnswer
+          flashWord={flashWord}
+          compareAnswer={compareAnswer}
+          correct={correct}
+          handleNextWord={handleNextWord}
+          counter={counter}
+          clicked={clicked}
+          setClicked={setClicked}
+          letterCount={letterCount}
+          setLetterCount={setLetterCount}
+        />
+      )}
     </StyledDiv>
   );
 }
